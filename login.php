6e55a8b5c1f339f6b68d34d0fe9cee81b51e7d65
Yesica-Viatri <yesicaviatri@outlook.com>
2023-08-14 18:16:59 -0300

    pizzas php

---
+++ login.php
@@ -0,0 +1,14 @@
+<?php
+$usr=$_POST ["username"];
+$pass=$_POST["password"];
+if (((strcmp($usr,"maria")==0)||(strcmp($usr,"maria@twitter.com")==0)) && (strcmp($pass,"123456")==0)){
+?>
+<h1>"Bienvenido"</h1>
+<?php
+;}
+    else{
+?>
+<h1>"Acceso denegado"</h1>
+<?php
+;}
+?>
